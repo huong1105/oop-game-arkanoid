40fd40ccacd2512eceac08100e3350d269d7f230
huong1105 <24021510@vnu.edu.vn>
2025-09-24 00:35:12 +0700

    1

---
+++ src/com/arkanoid/Main.java
@@ -0,0 +1,8 @@
+package com.arkanoid;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Game Arkanoid is starting...");
+        // Code để khởi tạo và chạy game sẽ được thêm vào đây sau.
+    }
+}
