f8d7058e49b64371d8fb5c1365a691e2abe43a9e
Ohhalo02 <hoang247742@gmail.com>
2025-10-03 20:04:07 +0700

    Update Renderer.java

--- src/com/arkanoid/ui/Renderer.java
+++ src/com/arkanoid/ui/Renderer.java
@@ -1,15 +1,51 @@
 package com.arkanoid.ui;
 
 import com.arkanoid.core.GameObject;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.List;
 
 public class Renderer {
-    public void draw(GameObject obj) {
-        // Logic để vẽ một GameObject.
-        // Ở phiên bản terminal, nó sẽ in ký tự ra console.
-        // Ở phiên bản GUI, nó sẽ vẽ hình ảnh lên panel.
+    private GraphicsContext gc;
+    private double canvasWidth;
+    private double canvasHeight;
+
+    public Renderer(GraphicsContext gc, double canvasWidth, double canvasHeight) {
+        this.gc = gc;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight =  canvasHeight;
     }
 
-    public void clearScreen() {
-        // Logic xóa màn hình để chuẩn bị cho khung hình mới
+    public void clear() {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, canvasWidth, canvasHeight);
+    }
+
+    /**
+     * Vẽ một đối tượng GameObject lên canvas.
+     * Giả sử mỗi GameObject đều có sprite (hình ảnh).
+     * @param obj Đối tượng cần vẽ.
+     */
+    public void draw(GameObject obj) {
+        if (obj.getSprite() != null) {
+            gc.drawImage(obj.getSprite(), obj.getX(), obj.getY(), obj.getWidth(), obj.getHeight());
+        } else {
+            gc.setFill(Color.WHITE);
+            gc.fillRect(obj.getX(), obj.getY(), obj.getWidth(), obj.getHeight());
+        }
+    }
+
+    /**
+     * Vẽ tất cả các đối tượng trong một danh sách.
+     * @param objects Danh sách các đối tượng cần vẽ.
+     */
+    public void render(List<GameObject> objects) {
+        clear();
+        for (GameObject obj : objects) {
+            if (obj.isActive()) {
+                draw(obj);
+            }
+        }
     }
 }
