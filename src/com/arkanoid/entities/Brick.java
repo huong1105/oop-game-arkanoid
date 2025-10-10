b06bc5d8c5bc5f9cf8530ecc7e3044f71e6d7d42
huong1105 <24021510@vnu.edu.vn>
2025-10-10 21:50:29 +0700

    Revert "test"
    This reverts commit af14ea9c9355a57be077b0dcb7e4dc5ec83de899.

--- src/com/arkanoid/entities/Brick.java
+++ src/com/arkanoid/entities/Brick.java
@@ -2,72 +2,24 @@ package com.arkanoid.entities;
 
 import com.arkanoid.core.GameObject;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
+
+import java.awt.*;
 
 public class Brick extends GameObject {
     private int hitPoints;
     private String type;
 
-    public Brick(int x, int y, int width, int height, int hitPoints, String type) {
+    public Brick(int x, int y, int width, int height, int hitPoints) {
         super(x, y, width, height);
         this.hitPoints = hitPoints;
-        this.type = type;
     }
 
     @Override
-    public void update() {
-        if (isDestroyed()) {
-            return;
-        }
-        // Có thể thêm logic animation hoặc hiệu ứng ở đây
-    }
+    public void update() { /* Logic cập nhật gạch */ }
 
     @Override
-    public void render(GraphicsContext gc) {
-        if (!isDestroyed()) {
-            // Chọn màu dựa trên type
-            switch (type != null ? type : "") {
-                case "HARD":
-                    gc.setFill(Color.BLUE);
-                    break;
-                case "SPECIAL":
-                    gc.setFill(Color.GREEN);
-                    break;
-                default:
-                    gc.setFill(Color.RED);
-                    break;
-            }
-            gc.fillRect(x, y, width, height);
-            gc.setStroke(Color.BLACK);
-            gc.strokeRect(x, y, width, height);
-        }
-    }
+    public void render(GraphicsContext gc) { /* Logic vẽ gạch */ }
 
-    public void takeHit() {
-        this.hitPoints--;
-    }
-
-    public boolean isDestroyed() {
-        return this.hitPoints <= 0;
-    }
-
-    public int getScore() {
-        return switch (type != null ? type : "") {
-            case "HARD" -> 200;
-            case "SPECIAL" -> 500;
-            default -> 100;
-        };
-    }
-
-    public int getHitPoints() {
-        return hitPoints;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
+    public void takeHit() { this.hitPoints--; }
+    public boolean isDestroyed() { return this.hitPoints <= 0; }
 }
