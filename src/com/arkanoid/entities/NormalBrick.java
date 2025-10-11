25cad677ce438c8dde80f622b94841c87a013f24
minhkhiem876 <minhkhiem2k6@gmail.com>
2025-10-12 00:14:09 +0700

    normal brick

--- src/com/arkanoid/entities/NormalBrick.java
+++ src/com/arkanoid/entities/NormalBrick.java
@@ -2,6 +2,6 @@ package com.arkanoid.entities;
 
 public class NormalBrick extends Brick {
     public NormalBrick(int x, int y, int width, int height) {
-        super(x, y, width, height, 1); // Cần 1 hit để vỡ
+        super(x, y, width, height, Const.BrickType.NORMAL);
     }
 }
