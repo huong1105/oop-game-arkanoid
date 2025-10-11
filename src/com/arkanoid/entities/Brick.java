c0edc02d7d94aba9712a277ee0485a9ee66b4849
minhkhiem876 <minhkhiem2k6@gmail.com>
2025-10-12 00:11:39 +0700

    brick.java

--- src/com/arkanoid/entities/Brick.java
+++ src/com/arkanoid/entities/Brick.java
@@ -2,24 +2,71 @@ package com.arkanoid.entities;
 
 import com.arkanoid.core.GameObject;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 import java.awt.*;
 
 public class Brick extends GameObject {
     private int hitPoints;
-    private String type;
+    private int maxHitPoints;
+    private Const.BrickType type;
 
-    public Brick(int x, int y, int width, int height, int hitPoints) {
+    public Brick(int x, int y, int width, int height, Const.BrickType type) {
         super(x, y, width, height);
-        this.hitPoints = hitPoints;
+        this.hitPoints = type.getHitPoints();
+        this.maxHitPoints = type.getHitPoints();
+        this.type = type;
     }
 
     @Override
-    public void update() { /* Logic cập nhật gạch */ }
+    public void update() {
+        if (hitPoints <= 0) {
+            setActive(false);
+        }
+    }
 
     @Override
-    public void render(GraphicsContext gc) { /* Logic vẽ gạch */ }
+    public void render(GraphicsContext gc) {
+        if (!isActive()) {
+            return;
+        }
+        switch (type) {
+            case NORMAL:
+                gc.setFill(Color.RED);
+            case HARD:
+                double intensity = (double) hitPoints / maxHitPoints;
+                gc.setFill(Color.rgb(128, 128, 128, intensity));
+            case SPECIAL:
+                gc.setFill(Color.GOLD);
+            default:
+                gc.setFill(Color.WHITE);
+        }
+        gc.fillRect(x, y, width, height);
+    }
 
-    public void takeHit() { this.hitPoints--; }
-    public boolean isDestroyed() { return this.hitPoints <= 0; }
+    public boolean takeHit() {
+        if (hitPoints > 0) {
+            hitPoints--;
+            if (hitPoints <= 0) {
+                setActive(false);
+                return true; // destroyed, trả về kiểu bool để xử lý cộng điểm.
+            }
+        }
+        return false;
+    }
+    public boolean isDestroyed() {
+        return this.hitPoints <= 0;
+    }
+
+    public int getScoreValue() {
+        return type.getScoreValue();
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public Const.BrickType getType() {
+        return type;
+    }
 }
