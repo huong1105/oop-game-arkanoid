cc84b04faa9b95b380d6917daa6499208be7a42d
minhkhiem876 <minhkhiem2k6@gmail.com>
2025-10-12 00:16:14 +0700

    strong brick

--- src/com/arkanoid/entities/StrongBrick.java
+++ src/com/arkanoid/entities/StrongBrick.java
@@ -2,6 +2,6 @@ package com.arkanoid.entities;
 
 public class StrongBrick extends Brick {
     public StrongBrick(int x, int y, int width, int height) {
-        super(x, y, width, height, 2); // Cần 2 hit để vỡ
+        super(x, y, width, height, Const.BrickType.HARD); // Cần 2 hit để vỡ
     }
 }
