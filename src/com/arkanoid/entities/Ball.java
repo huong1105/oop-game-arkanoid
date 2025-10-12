630ed11192b7d2d4f05d96d842c53730935fc7a9
tangerinebetter <hieubeo264@gmail.com>
2025-10-12 17:04:51 +0700

    sửa hằng số và thêm khởi tạo mặc định

--- src/com/arkanoid/entities/Ball.java
+++ src/com/arkanoid/entities/Ball.java
@@ -2,17 +2,18 @@ package com.arkanoid.entities;
 
 import com.arkanoid.Const;
 import com.arkanoid.core.MovableObject;
-import com.arkanoid.core.GameObject;
 import javafx.scene.canvas.GraphicsContext;
-
-import java.awt.*;
 
 // Lớp Ball kế thừa từ MovableObject
 public class Ball extends MovableObject {
-    private int maxSpeed;
+    private int maxSpeed = Const.INSTANCE.getBallMaxSpeed();
 
     public Ball(int x, int y, int diameter, int speedX, int speedY) {
         super(x, y, diameter, diameter, speedX, speedY);
+    }
+
+    public Ball() {
+        super(0, 0, Const.INSTANCE.getBallDiameter(), Const.INSTANCE.getBallDiameter(), 0, 0);
     }
 
     public int getMaxSpeed() {
@@ -28,23 +29,23 @@ public class Ball extends MovableObject {
      */
     @Override
     public void update() {
-        while (getSpeedX() * getSpeedX() + getSpeedY() * getSpeedY() > maxSpeed){
+        while (getSpeedX() * getSpeedX() + getSpeedY() * getSpeedY() > maxSpeed) {
             setSpeedX(getSpeedX() * 0.99);
             setSpeedY(getSpeedY() * 0.99);
         }
-        setX(getX() + (int)getSpeedX());
-        setY(getY() + (int)getSpeedY());
+        setX(getX() + (int) getSpeedX());
+        setY(getY() + (int) getSpeedY());
     }
 
     @Override
     public void render(GraphicsContext gc) { /* Logic vẽ bóng */ }
 
     public void reset(Paddle paddle) {
-        x = (Const.INSTANCE.getScreenWidth() / 2) - (Const.INSTANCE.getBallDiameter() / 2);
-        y = paddle.getY() - Const.INSTANCE.getBallDiameter();
+        x = Const.INSTANCE.getBallDefaultPosX();
+        y = Const.INSTANCE.getBallDefaultPosY();
         width = Const.INSTANCE.getBallDiameter();
         height = Const.INSTANCE.getBallDiameter();
-        speedX =  Const.INSTANCE.getBallSpeedX();
+        speedX = Const.INSTANCE.getBallSpeedX();
         speedY = Const.INSTANCE.getBallSpeedY();
     }
 
