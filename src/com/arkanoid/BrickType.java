ae8a7de4a5b5cfecdfab75371a545825c455a7c3
minhkhiem876 <minhkhiem2k6@gmail.com>
2025-10-12 12:34:04 +0700

    them wall

--- src/com/arkanoid/BrickType.java
+++ src/com/arkanoid/BrickType.java
@@ -3,7 +3,8 @@ package com.arkanoid;
 public enum BrickType {
     NORMAL(1, 10),  // 1 hit để phá, 10 điểm
     HARD(2, 20),    // 2 hits để phá, 20 điểm
-    SPECIAL(3, 50); // 1 hit, 50 điểm, drop power-up
+    SPECIAL(3, 50), // 1 hit, 50 điểm, drop power-up
+    WALL(1000, 1000); // tường nên không cần quan tâm takeHits;
 
     private final int hitPoints;
     private final int scoreValue;
---
+++ src/com/arkanoid/entities/Wall.java
@@ -0,0 +1,9 @@
+package com.arkanoid.entities;
+
+import com.arkanoid.BrickType;
+
+public class Wall extends Brick {
+    public Wall(int x, int y, int width, int height) {
+        super(x, y, width, height, BrickType.WALL);
+    }
+}
