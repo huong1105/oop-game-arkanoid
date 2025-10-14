e27626a66c809fa385f0d1862d5cf940190e194a
huong1105 <24021510@vnu.edu.vn>
2025-10-14 15:45:42 +0700

    update phương thức setupInput điều khiển paddle
    xây dựng phương thức để điều khiển paddle trái phải
    
    Co-Authored-By: tangerinebetter <198608955+tangerinebetter@users.noreply.github.com>

--- src/com/arkanoid/ui/KeyInput.java
+++ src/com/arkanoid/ui/KeyInput.java
@@ -1,8 +1,7 @@
 package com.arkanoid.ui;
 
-import com.arkanoid.Const;
+import com.arkanoid.entities.Paddle;
 import com.arkanoid.game.GameManager;
-import com.arkanoid.game.GameState;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 
@@ -11,6 +10,44 @@ public class KeyInput {
 
     public static void setupInput(Scene scene) {
         GameManager gm = GameManager.getInstance();
+        scene.setOnKeyPressed(event -> {
+            System.out.println("Key Pressed: " + event.getCode());
+            // ==========================================================
+            Paddle paddle = gm.getPaddle();
+            if (paddle == null) return;
+            KeyCode code = event.getCode();
+            switch (code) {
+                case LEFT:
+                case A:
+                    paddle.setMovingLeft(true);
+                    paddle.setMovingRight(false);
+                    break;
+                case RIGHT:
+                case D:
+                    paddle.setMovingRight(true);
+                    paddle.setMovingLeft(false);
+                    break;
+                default:
+                    break;
+            }
+        });
+        scene.setOnKeyReleased(event -> {
+            Paddle paddle = gm.getPaddle();
+            if (paddle == null) return;
 
+            KeyCode code = event.getCode();
+            switch (code) {
+                case LEFT:
+                case A:
+                    paddle.setMovingLeft(false);
+                    break;
+                case RIGHT:
+                case D:
+                    paddle.setMovingRight(false);
+                    break;
+                default:
+                    break;
+            }
+        });
     }
 }
