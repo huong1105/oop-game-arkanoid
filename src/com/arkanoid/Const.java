c0d72f2faaa543ee1ceb400c2b6b8eb2153c226a
huong1105 <24021510@vnu.edu.vn>
2025-10-14 15:39:33 +0700

    Sửa lỗi chính tả

--- src/com/arkanoid/Const.java
+++ src/com/arkanoid/Const.java
@@ -5,8 +5,8 @@ public final class Const {
     }
 
     public static final int SCREEN_WIDTH = 1000;
-    public static final int SCREEN_HEGHT = 800;
-    public static final int BALL_MAXSPEED = 15;
+    public static final int SCREEN_HEIGHT = 800;
+    public static final int BALL_MAXSPEED = 30;
     public static final int PADDLE_WIDTH = 120;
     public static final int PADDLE_HEIGHT = 20;
     public static final int BALL_DIAMETER = 20;
@@ -14,9 +14,9 @@ public final class Const {
     public static final int BALL_SPEEDY = 5;
     public static final int PADDLE_DEFAULT_SPEED = 10;
     public static final int PADDLE_DEFAULT_POS_X = SCREEN_WIDTH / 2 - PADDLE_WIDTH / 2;
-    public static final int PADDLE_DEFAULT_POS_Y = SCREEN_HEGHT - PADDLE_HEIGHT;
+    public static final int PADDLE_DEFAULT_POS_Y = SCREEN_HEIGHT - PADDLE_HEIGHT - 20;
     public static final int BALL_DEFAULT_POS_X = SCREEN_WIDTH / 2 - BALL_DIAMETER / 2;
-    public static final int BALL_DEFAULT_POS_Y = PADDLE_DEFAULT_POS_Y + BALL_DIAMETER;
+    public static final int BALL_DEFAULT_POS_Y = PADDLE_DEFAULT_POS_Y - BALL_DIAMETER -10;
 
     public static final int DEFAULT_LIVES = 3;
     public static final int DEFAULT_SCORES = 0;
