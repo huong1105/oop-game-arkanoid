ed34eff7e8ae0b794ef441205ab898fdbaea877b
minhkhiem876 <minhkhiem2k6@gmail.com>
2025-10-18 00:26:00 +0700

    sua mot ti

--- src/com/arkanoid/BrickType.java
+++ src/com/arkanoid/BrickType.java
@@ -4,7 +4,7 @@ public enum BrickType {
     NORMAL(1, 10),  // 1 hit để phá, 10 điểm
     HARD(2, 20),    // 2 hits để phá, 20 điểm
     SPECIAL(3, 50), // 1 hit, 50 điểm, drop power-up
-    WALL(1000, 1000); // tường nên không cần quan tâm takeHits;
+    WALL(10000000, 10000000); // tường nên không cần quan tâm takeHits;
 
     private final int hitPoints;
     private final int scoreValue;
