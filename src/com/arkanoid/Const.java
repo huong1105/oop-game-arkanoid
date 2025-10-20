2e92436a6b13d9d056e0926bf3398b6ae59a90e7
tangerinebetter <hieubeo264@gmail.com>
2025-10-20 15:29:35 +0700

    sửa kích cỡ gạch

--- src/com/arkanoid/Const.java
+++ src/com/arkanoid/Const.java
@@ -17,8 +17,8 @@ public final class Const {
     public static final int PADDLE_DEFAULT_POS_Y = SCREEN_HEIGHT - PADDLE_HEIGHT - 20;
     public static final int BALL_DEFAULT_POS_X = SCREEN_WIDTH / 2 - BALL_DIAMETER / 2;
     public static final int BALL_DEFAULT_POS_Y = PADDLE_DEFAULT_POS_Y - BALL_DIAMETER - 10;
-    public static final int BRICK_WIDTH = 100;
-    public static final int BRICK_HEIGHT = 50;
+    public static final int BRICK_WIDTH = 60;
+    public static final int BRICK_HEIGHT = 25;
 
     public static final int DEFAULT_LIVES = 3;
     public static final int DEFAULT_SCORES = 0;
