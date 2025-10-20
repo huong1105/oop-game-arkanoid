47c26120bfee5d30cf065c27f6638eb69a2e44fd
huong1105 <24021510@vnu.edu.vn>
2025-10-21 03:05:15 +0700

    chia màn hình thành 2 phần

--- src/com/arkanoid/Const.java
+++ src/com/arkanoid/Const.java
@@ -6,6 +6,8 @@ public final class Const {
 
     public static final int SCREEN_WIDTH = 1000;
     public static final int SCREEN_HEIGHT = 800;
+    public static final double INFO_PANEL_WIDTH = 200.0;
+    public static final double WINDOW_WIDTH = SCREEN_WIDTH + INFO_PANEL_WIDTH;
     public static final double BALL_MAXSPEED = 130000;
     public static final int PADDLE_WIDTH = 120;
     public static final int PADDLE_HEIGHT = 20;
--- src/com/arkanoid/Main.java
+++ src/com/arkanoid/Main.java
@@ -5,10 +5,17 @@ import com.arkanoid.ui.KeyInput;
 import com.arkanoid.ui.Renderer;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,14 +24,26 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         GameManager gm = GameManager.getInstance();
-        Pane root = new Pane();
+
+        BorderPane root = new BorderPane();
+
         Canvas canvas = new Canvas(Const.SCREEN_WIDTH, Const.SCREEN_HEIGHT);
-        root.getChildren().add(canvas);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        root.setCenter(canvas);
+
+        Pane infoPanel = new Pane();
+        infoPanel.setPrefWidth(Const.INFO_PANEL_WIDTH);
+        infoPanel.setStyle(
+                "-fx-background-color: #000000;" +
+                        "-fx-border-color: white;" +
+                        "-fx-border-width: 0 0 0 2;" +
+                        "-fx-border-style: solid;"
+        );
+        root.setRight(infoPanel);
 
         Renderer renderer = new Renderer(gc, canvas.getWidth(), canvas.getHeight());
 
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, Const.WINDOW_WIDTH, Const.SCREEN_HEIGHT);
         primaryStage.setTitle("Arkanoid - JavaFX");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -33,6 +52,7 @@ public class Main extends Application {
         KeyInput.setupInput(scene);
 
         lastFrameTime = System.nanoTime();
+
         AnimationTimer gameLoop = new AnimationTimer() {
             @Override
             public void handle(long now) {
