62c90e0c7856f915bc856b3cc59474d949316a3c
tangerinebetter <hieubeo264@gmail.com>
2025-10-22 13:58:59 +0700

    Thêm powerup shield
    thêm powerup khiên để chống rơi

---
+++ src/com/arkanoid/entities/ShieldPowerUp.java
@@ -0,0 +1,31 @@
+package com.arkanoid.entities;
+
+import com.arkanoid.BrickType;
+import com.arkanoid.Const;
+import com.arkanoid.game.GameManager;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class ShieldPowerUp extends PowerUp {
+    private Shield shield;
+    private static final int DURATION = 0;
+
+    public ShieldPowerUp(int x, int y) {
+        super(x, y, 20, 20, "Shield", DURATION);
+        shield = new Shield();
+    }
+
+    @Override
+    public void applyEffect() {
+        GameManager gm = GameManager.getInstance();
+        if (gm.getGameObjects().contains(shield)) {
+            gm.getGameObjects().remove(shield);
+        }
+        gm.addGameObject(shield);
+    }
+
+    @Override
+    public void removeEffect() {
+        // Không làm gì vì hiệu lực vĩnh viễn
+    }
+}
